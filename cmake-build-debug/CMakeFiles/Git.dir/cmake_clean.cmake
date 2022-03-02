6150d97f854cccbee07624986ca9467ecf56f900
Łukasz Szuligowski <lszuligowski@gmail.com>
2022-03-02 15:32:12 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Git.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Git.dir/main.c.o"
+  "Git"
+  "Git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
